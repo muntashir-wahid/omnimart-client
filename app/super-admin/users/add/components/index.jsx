a4384cea843b93c32fb9bbe9585d6be041534848
muntashir-wahid <muntashir.wahid@yahoo.com>
2024-10-29 19:34:24 +0600

    implemented user add functionality

---
+++ app/super-admin/users/add/components/index.jsx
@@ -0,0 +1,153 @@
+"use client";
+
+import { useFormik } from "formik";
+import { useRouter } from "next/navigation";
+import { object, string } from "yup";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import FormError from "@/components/shared/Form/FormError";
+import APIKit from "@/lib/apiKit";
+
+const registerSchema = object({
+  firstName: string().required("First Name is Required"),
+  lastName: string().required("Last Name is Required"),
+  phone: string().required("Phone Number is Required"),
+  email: string().required("Email is Required").email("Use a valid email"),
+  password: string()
+    .required("Password is required")
+    .min(6, "Password should be more then 6 characters"),
+});
+
+const userRoles = [
+  { label: "User", value: "USER" },
+  { label: "Admin", value: "ADMIN" },
+  { label: "Super Admin", value: "SUPER_ADMIN" },
+];
+
+const SuperAdminAddUserModule = () => {
+  const router = useRouter();
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      password: "",
+      userRole: "USER",
+    },
+    validationSchema: registerSchema,
+    onSubmit: async (values, action) => {
+      console.log(values);
+      try {
+        const { data } = await APIKit.users.addUser(values);
+
+        console.log(data);
+        router.push("/super-admin/users");
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  });
+
+  return (
+    <div className="w-full max-w-2xl mx-auto my-6 bg-gray-50 py-10 px-6 md:px-10 lg:px-16 rounded-md shadow-lg">
+      <form onSubmit={formik.handleSubmit} className="flex flex-col gap-4">
+        <div className="space-y-1">
+          <Label htmlFor="firstName">First Name</Label>
+          <Input
+            type="text"
+            id="firstName"
+            placeholder="User First Name..."
+            onChange={formik.handleChange}
+          />
+          <FormError formik={formik} name="firstName" />
+        </div>
+
+        <div className="space-y-1">
+          <Label htmlFor="lastName">Last Name</Label>
+          <Input
+            type="text"
+            id="lastName"
+            placeholder="User Last Name..."
+            onChange={formik.handleChange}
+          />
+          <FormError formik={formik} name="lastName" />
+        </div>
+
+        <div className="space-y-1">
+          <Label htmlFor="email">Email</Label>
+          <Input
+            type="email"
+            id="email"
+            placeholder="User Email..."
+            onChange={formik.handleChange}
+          />
+          <FormError formik={formik} name="email" />
+        </div>
+
+        <div className="space-y-1">
+          <Label htmlFor="phone">Phone</Label>
+          <Input
+            type="tel"
+            id="phone"
+            placeholder="User Phone..."
+            onChange={formik.handleChange}
+          />
+          <FormError formik={formik} name="phone" />
+        </div>
+
+        <div className="space-y-1">
+          <Label htmlFor="password">Password</Label>
+          <Input
+            type="password"
+            id="password"
+            placeholder="XXXXXXXXX"
+            onChange={formik.handleChange}
+          />
+          <FormError formik={formik} name="password" />
+        </div>
+
+        <div className="space-y-1">
+          <Label htmlFor="email">User Role</Label>
+          <Select
+            value={formik.values.userRole}
+            onValueChange={(selectedRole) => {
+              formik.setFieldValue("userRole", selectedRole);
+            }}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="Select a Role" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Roles</SelectLabel>
+                {userRoles.map((role) => (
+                  <SelectItem key={role.value} value={role.value}>
+                    {role.label}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+
+        <Button type="submit">Add User</Button>
+      </form>
+    </div>
+  );
+};
+
+export default SuperAdminAddUserModule;
--- app/super-admin/users/add/page.jsx
+++ app/super-admin/users/add/page.jsx
@@ -1,16 +1,18 @@
 import Link from "next/link";
 
 import { ArrowLeft } from "lucide-react";
+import SuperAdminAddUserModule from "./components";
 
 const SuperAdminAddUserPage = () => {
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <header className="flex items-center gap-2">
         <Link href="/super-admin/users">
           <ArrowLeft />
         </Link>
         <h1 className="text-3xl font-semibold text-gray-800">Add New User</h1>
       </header>
+      <SuperAdminAddUserModule />
     </div>
   );
 };
