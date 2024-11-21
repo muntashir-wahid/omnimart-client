e64c2c6756501d2940a9149dbc5c348d230640ce
muntashir-wahid <muntashir.wahid@yahoo.com>
2024-11-21 15:32:42 +0600

    implemented inventory stock search feature

--- app/admin/inventory/[inventorySlug]/components/InventoryStock.jsx
+++ app/admin/inventory/[inventorySlug]/components/InventoryStock.jsx
@@ -1,14 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { Plus } from "lucide-react";
 
 import APIKit from "@/lib/apiKit";
+import { sanitizeParams } from "@/lib/utils";
 
 import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
 
-import DataLoadingState from "@/components/shared/Loaders/DataLoadingState";
-import StockItem from "./StockItem";
 import AddNewStock from "./AddNewStock";
+import DataLoadingState from "@/components/shared/Loaders/DataLoadingState";
+import SearchField from "@/components/shared/Form/SearchField";
+import StockItem from "./StockItem";
 
 const InventoryStock = ({ product }) => {
   const [openStockAddModal, setOpenStockAddModal] = useState(false);
@@ -19,16 +22,31 @@ const InventoryStock = ({ product }) => {
     category: { uid: categoryUid },
   } = product;
 
+  const [params, setParams] = useState({ search: "" });
+
   const { data, isLoading, refetch } = useQuery({
     queryKey: [`inventory/${uid}/stocks`],
-    queryFn: () => APIKit.inventory.stock.getInventoryAllStocks(uid),
+    queryFn: () =>
+      APIKit.inventory.stock.getInventoryAllStocks(uid, sanitizeParams(params)),
   });
+
+  useEffect(() => {
+    refetch();
+  }, [params]);
 
   if (isLoading) {
     return <DataLoadingState content="Inventory Stocks is Loading..." />;
   }
 
   const { stocks } = data.data;
+
+  const handleParamsChange = (value, fieldName) => {
+    setParams((prevParams) => {
+      const newParams = { ...prevParams };
+      newParams[fieldName] = value;
+      return newParams;
+    });
+  };
 
   return (
     <>
@@ -41,6 +59,21 @@ const InventoryStock = ({ product }) => {
             <Plus />
             <span className="hidden sm:inline-block">Add New Stock</span>
           </Button>
+        </div>
+
+        <div className="flex flex-col md:flex-row gap-6">
+          <div className="space-y-2 w-full">
+            <Label htmlFor="search">Search Stock</Label>
+            <SearchField
+              id="search"
+              name="search"
+              value={params.search}
+              onChange={(event) =>
+                handleParamsChange(event.target.value, "search")
+              }
+              placeholder="Search by SKU..."
+            />
+          </div>
         </div>
 
         <div className="flex flex-col gap-4">
